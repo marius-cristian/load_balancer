83f552f53c469e484d12b0658a76836b2a6e9536
mfc <cristian.marius.florin@protonmail.com>
2020-06-28 13:20:10 +0200

    example with more tasks than workers

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,33 @@ async fn main() {
     let (tx, mut rx) = mpsc::channel(100);
     let (tx1, mut rx1) = mpsc::channel(100);
     let (tx2, mut rx2) = mpsc::channel(100);
+    let (tx3, mut rx3) = mpsc::channel(100);
+    let (tx4, mut rx4) = mpsc::channel(100);
+    let (tx5, mut rx5) = mpsc::channel(100);
 
     lb.assign_task(tx).await;
     lb.assign_task(tx1).await;
     lb.assign_task(tx2).await;
+    lb.assign_task(tx3).await;
+    lb.assign_task(tx4).await;
+    lb.assign_task(tx5).await;
 
 
     let received = rx.recv().await.unwrap();
     let received1 = rx1.recv().await.unwrap();
     let received2 = rx2.recv().await.unwrap();
+    let received3 = rx3.recv().await.unwrap();
+    let received4 = rx4.recv().await.unwrap();
+    let received5 = rx5.recv().await.unwrap();
 
 
     println!("Got: {}", received);
     println!("Got: {}", received1);
     println!("Got: {}", received2);
+    println!("Got: {}", received3);
+    println!("Got: {}", received4);
+    println!("Got: {}", received5);
+
 
     // Alternative using the client send work; but there is an error
     // send_work(lb);
